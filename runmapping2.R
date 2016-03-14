c2fdc5722a0630fd566c8493c630b881f4ca584b
diazrenata <diaz.renatam@gmail.com>
2016-03-15 08:30:44 +1000

    Plotting fixed

--- runmapping2.R
+++ runmapping2.R
@@ -6,11 +6,13 @@ x <- c(-5, 10, 5, 10, 10, 0, -5, 0, -5, 10, 5, 10, 9, 10)
 maptrees(x)
 
 # Read data from .csv
-# File must have 6 columns: 
-# Known left x, Known left y, Known right x, Known right y, Distance to left target, Distance to right target
-datain <- read.csv("mappingtest3.csv", header=FALSE)
+# File must have 14 columns: 
+# First 8 are x,y coordinates of grid cell corners
+# Next 6 are: Known left x, Known left y, Known right x, Known right y, Distance to left target, Distance to right target
+datain <- read.csv("maptesta2b3.csv", header=FALSE)
 
-
+# Read corners coordinates and labels from .csv
+corners <- read.csv("a2b3corners.csv")
 
 # How many rows? (= how many times to run function)
 repeats <- nrow(datain)
@@ -37,35 +39,14 @@ dataout
 
 # Place mapped trees on a plot
 library(ggplot2)
+library(ggrepel)
 
-cornersxa <- c(datain[1,1], datain[1,3], datain[1,5], datain[1,7])
-cornersya <- c(datain[1,2], datain[1,4], datain[1,6], datain[1,8])
-cornerslabsa <- c(1:4)
-cornersx <- as.vector(cornersxa, mode="numeric")
-cornersy <- as.vector(cornersya, mode="numeric")
-cornerslabs <-as.vector(cornerslabsa, mode="numeric")
-cornersbind <- cbind(cornersx, cornersy, cornerslabs)
-cornerstoplot <- as.data.frame(cornersbind)
+forplot <- as.data.frame(dataout[1:repeats,1:3])
 
-forplot <- as.data.frame(dataout[1:28,1:3])
-# forplot
 visualmap <- ggplot(forplot) + geom_point(aes(UnknownX, UnknownY)) + 
   geom_text(aes(UnknownX, UnknownY, label=Tag)) +
   theme_bw()+
-  geom_text(data=cornerstoplot, aes(cornersx, cornersy, label=cornerslabs), size=6)
+  geom_point(data=corners, aes(cornerx, cornery, label=cornerpt), size=3, color="blue") +
+  geom_text(data=corners, aes(cornerx, cornery, label=cornerpt), size=6, color="blue")
 visualmap
 
-# Add the grid cell corners
-cornersxa <- c(datain[1,1], datain[1,3], datain[1,5], datain[1,7])
-cornersya <- c(datain[1,2], datain[1,4], datain[1,6], datain[1,8])
-cornerslabsa <- c(1:4)
-cornersx <- as.vector(cornersxa, mode="numeric")
-cornersy <- as.vector(cornersya, mode="numeric")
-cornerslabs <-as.vector(cornerslabsa, mode="numeric")
-cornersbind <- cbind(cornersx, cornersy, cornerslabs)
-cornerstoplot <- as.data.frame(cornersbind)
-cornersmapped <-ggplot(data=cornerstoplot, aes(x=cornersx, y=cornersy, label=cornerslabs)) + geom_text(size = 10) + geom_point(size = 2)
-cornersmapped
-visualmap <- cornersmapped + geom_point(data=forplot, aes(x=UnknownX, y=UnknownY, label=Tag)) +
-  geom_text(data=forplot, aes(x=UnknownX, y=UnknownY, label=Tag))
-visualmap
